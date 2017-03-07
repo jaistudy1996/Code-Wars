72da4a9179f1e4d020ff82c7e5d75b6d1d998971
Jayant Arora <jaistudy1996@gmail.com>
2017-03-07 18:03:43 -0500

    JS fundamental ques

---
+++ JavaScript/11.js
@@ -0,0 +1,16 @@
+/*
+Complete the bool_to_word (Javascript: boolToWord ) method.
+
+Given: a boolean value
+
+Return: a 'Yes' string for true and a 'No' string for false
+*/
+
+function boolToWord( bool ){
+  if(bool == true){
+    return 'Yes';
+  }
+  else{
+    return 'No';
+  }
+}
